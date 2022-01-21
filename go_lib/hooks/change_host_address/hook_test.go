085ab60e509f6769c8ca5f65447005032f796b06
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2022-01-21 20:02:23 +0300

    simplify mockging

--- go_lib/hooks/change_host_address/hook_test.go
+++ go_lib/hooks/change_host_address/hook_test.go
@@ -17,29 +17,24 @@ limitations under the License.
 package change_host_address
 
 import (
+	"testing"
+
 	"github.com/deckhouse/deckhouse/go_lib/set"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func Test_changeHostAddress(t *testing.T) {
-	mock := newMockPodClient()
 
-	type args struct {
-		podClient podClient
-		podHosts  []podHost
-	}
 	tests := []struct {
-		name   string
-		args   args
-		assert func(*testing.T, *mockPodClient)
+		name     string
+		podHosts []podHost
+		assert   func(*testing.T, *mockPodClient)
 	}{
 		{
 			name: "does nothing when no pods available",
-			args: args{
-				podClient: mock,
-				podHosts:  []podHost{},
-			},
+
+			podHosts: []podHost{},
+
 			assert: func(t *testing.T, client *mockPodClient) {
 				assert.Equal(t, client.deletedName.Size(), 0)
 				assert.Equal(t, client.annotatedName.Size(), 0)
@@ -48,89 +43,96 @@ func Test_changeHostAddress(t *testing.T) {
 		},
 		{
 			name: "ignores pod without IP",
-			args: args{
-				podClient: mock,
-				podHosts: []podHost{{
-					Name:      "a",
-					IP:        "",
-					InitialIP: "1.2.3.4",
-				}},
-			},
+
+			podHosts: []podHost{{
+				Name:      "a",
+				IP:        "",
+				InitialIP: "1.2.3.4",
+			}},
+
 			assert: func(t *testing.T, client *mockPodClient) {
 				assert.Equal(t, 0, client.deletedName.Size())
 				assert.Equal(t, 0, client.annotatedName.Size())
-				assert.Equal(t, 0, client.annotatedHost.Size())
 			},
 		},
 		{
 			name: "annotates Pod with IP when Pod does not have the annotation",
-			args: args{
-				podClient: mock,
-				podHosts: []podHost{{
-					Name:      "a",
-					IP:        "1.2.3.4",
-					InitialIP: "",
-				}},
-			},
+
+			podHosts: []podHost{{
+				Name:      "a",
+				IP:        "1.2.3.4",
+				InitialIP: "",
+			}},
+
 			assert: func(t *testing.T, client *mockPodClient) {
 				assert.Equal(t, 0, client.deletedName.Size())
-				assert.True(t, client.annotatedHost.Has("1.2.3.4"), "expected IP from the Pod")
+
 				assert.True(t, client.annotatedName.Has("a"), "expected name from the Pod")
+				assert.True(t, client.annotatedHost.Has("1.2.3.4"), "expected IP from the Pod")
 			},
 		},
 		{
 			name: "deletes Pod when IP and initial IP do not equal",
-			args: args{
-				podClient: mock,
-				podHosts: []podHost{{
-					Name:      "a",
-					IP:        "1.2.3.4",
-					InitialIP: "4.3.2.1",
-				}},
-			},
+
+			podHosts: []podHost{{
+				Name:      "a",
+				IP:        "1.2.3.4",
+				InitialIP: "4.3.2.1",
+			}},
+
 			assert: func(t *testing.T, client *mockPodClient) {
 				assert.Equal(t, 1, client.deletedName.Size())
 				assert.True(t, client.deletedName.Has("a"))
+
 				assert.Equal(t, 0, client.annotatedName.Size())
 				assert.Equal(t, 0, client.annotatedHost.Size())
 			},
 		},
 		{
 			name: "process multiple pods correclty",
-			args: args{
-				podClient: mock,
-				podHosts: []podHost{{
-					Name:      "annotated",
-					IP:        "1.2.3.4",
-					InitialIP: "",
-				}, {
-					Name:      "deleted",
-					IP:        "2.3.4.5",
-					InitialIP: "4.3.2.1",
-				}, {
-					Name:      "skipped",
-					IP:        "",
-					InitialIP: "",
-				}},
-			},
+
+			podHosts: []podHost{{
+				Name:      "no-anno",
+				IP:        "4.5.6.7",
+				InitialIP: "",
+			}, {
+				Name:      "badhost",
+				IP:        "2.3.4.5",
+				InitialIP: "4.3.2.1",
+			}, {
+				Name:      "skipped",
+				IP:        "",
+				InitialIP: "",
+			}, {
+				Name:      "no-anno-2",
+				IP:        "4.5.6.8",
+				InitialIP: "",
+			}},
+
 			assert: func(t *testing.T, client *mockPodClient) {
 				assert.Equal(t, 1, client.deletedName.Size())
-				assert.True(t, client.deletedName.Has("deleted"))
+				assert.True(t, client.deletedName.Has("badhost"))
 
-				assert.Equal(t, 1, client.annotatedName.Size())
-				assert.Equal(t, 1, client.annotatedHost.Size())
-				assert.True(t, client.annotatedName.Has("annotated"))
-				assert.True(t, client.annotatedHost.Has("1.2.3.4"))
+				assert.Equal(t, 2, client.annotatedName.Size())
+				assert.Equal(t, 2, client.annotatedHost.Size())
+
+				assert.True(t, client.annotatedName.Has("no-anno"))
+				assert.True(t, client.annotatedHost.Has("4.5.6.7"))
+
+				assert.True(t, client.annotatedName.Has("no-anno-2"))
+				assert.True(t, client.annotatedHost.Has("4.5.6.8"))
 			},
 		},
 	}
+
+	clientMock := newMockPodClient()
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			mock := tt.args.podClient.(*mockPodClient)
-			defer mock.clean()
+			defer clientMock.clean()
 
-			changeHostAddress(tt.args.podClient, tt.args.podHosts)
-			tt.assert(t, mock)
+			changeHostAddress(clientMock, tt.podHosts)
+
+			tt.assert(t, clientMock)
 		})
 	}
 }
